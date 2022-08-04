81b98b11fb609357d2fe1db6759384659cb4ae51
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-04 21:57:18 +0300

    5.10

--- js/scripts.js
+++ js/scripts.js
@@ -1,177 +1,194 @@
-// 5.1
+// // 5.1
 
-const pizzaPalace = {
-  pizzas: ["Supercheese", "Smoked", "Four meats"],
-  // Change code below this line
-  checkPizza(pizzaName) {
-    return this.pizzas.includes(pizzaName);
-  },
-  order(pizzaName) {
-    const isPizzaAvailable = this.checkPizza(pizzaName);
+// const pizzaPalace = {
+//   pizzas: ["Supercheese", "Smoked", "Four meats"],
+//   // Change code below this line
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
 
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
 
-    return `Order accepted, preparing «${pizzaName}» pizza`;
-  },
-  // Change code above this line
-};
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+//   // Change code above this line
+// };
 
-//   5.2
+// //   5.2
 
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Change code above this line
+// };
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
 
+// // 5.3
 
-// 5.3
+// const historyService = {
+//     orders: [
+//       { email: "jacob@hotmail.com", dish: "Burrito" },
+//       { email: "solomon@topmail.net", dish: "Burger" },
+//       { email: "artemis@coldmail.net", dish: "Pizza" },
+//       { email: "solomon@topmail.net", dish: "Apple pie" },
+//       { email: "jacob@hotmail.com", dish: "Taco" },
+//     ],
+//     // Change code below this line
+//     getOrdersLog() {
+//       return this.orders
+//         .map(order => `email: ${order.email} dish: ${order.dish}`)
+//         .join(" - ");
+//     },
+//     getEmails() {
+//       const emails = this.orders.map(order => order.email);
+//       const uniqueEmails = new Set(emails);
+//       return [...uniqueEmails];
+//     },
+//     getOrdersByEmail(email) {
+//       return this.orders.filter(order => order.email === email);
+//     },
+//     // Change code above this line
+//   };
 
-const historyService = {
-    orders: [
-      { email: "jacob@hotmail.com", dish: "Burrito" },
-      { email: "solomon@topmail.net", dish: "Burger" },
-      { email: "artemis@coldmail.net", dish: "Pizza" },
-      { email: "solomon@topmail.net", dish: "Apple pie" },
-      { email: "jacob@hotmail.com", dish: "Taco" },
-    ],
-    // Change code below this line
-    getOrdersLog() {
-      return this.orders
-        .map(order => `email: ${order.email} dish: ${order.dish}`)
-        .join(" - ");
-    },
-    getEmails() {
-      const emails = this.orders.map(order => order.email);
-      const uniqueEmails = new Set(emails);
-      return [...uniqueEmails];
-    },
-    getOrdersByEmail(email) {
-      return this.orders.filter(order => order.email === email);
-    },
-    // Change code above this line
-  };
+// //   5.4
 
- 
-//   5.4
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
 
-const parent = {
-  name: "Stacey",
-  surname: "Moore",
-  age: 54,
-  heritage: "Irish",
-};
-// Change code below this line
+//   const child = Object.create(parent);
 
-  const child = Object.create(parent);
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
 
-// Change code above this line
-child.name = "Jason";
-child.age = 27;
+// // 5.5
 
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
 
+//   const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
 
-// 5.5
+//   const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
 
-const ancestor = {
-  name: "Paul",
-  age: 83,
-  surname: "Dawson",
-  heritage: "Irish",
-};
-// Change code below this line
+// // Change code above this line
 
-  const parent = Object.create(ancestor);
-parent.name = "Stacey";
-parent.surname = "Moore";
-parent.age = 54;
+// // 5.6
 
-  const child = Object.create(parent);
-child.name = "Jason";
-child.age = 27;
+// class Car {};
 
-// Change code above this line
+// // 5.7
 
+// class Car {
+//   // Change code below this line
+//   constructor(brand, model, price) {
+// this.brand = brand;
+//   this.model = model;
+//   this.price = price;
 
-// 5.6
+//   }
+//   // Change code above this line
+// }
 
-class Car {};
+// // 5.8
+// class Car {
+//   // Change code below this line
+//   constructor({brand, model, price}) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
 
+// // 5.9
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+// getPrice () {
+// return this.price;
+// }
 
-// 5.7
+//   changePrice (newPrice) {
+//     this.price = newPrice;
+//   }
 
-class Car {
-  // Change code below this line
-  constructor(brand, model, price) {
-this.brand = brand;
-  this.model = model;
-  this.price = price;
-
-  }
-  // Change code above this line
-}
-
-// 5.8
-class Car {
-  // Change code below this line
-  constructor({brand, model, price}) {
-    this.brand = brand;
-    this.model = model;
-    this.price = price;
-  }
-  // Change code above this line
-}
-
-
-// 5.9
-class Car {
-  constructor({ brand, model, price }) {
-    this.brand = brand;
-    this.model = model;
-    this.price = price;
-  }
-  // Change code below this line
-getPrice () {
-return this.price;
-}
-
-  changePrice (newPrice) {
-    this.price = newPrice;
-  }
-
-  // Change code above this line
-}
-
+//   // Change code above this line
+// }
 
 // 5.10
+// class Storage {
+//   constructor( items ) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+
+//   addItem(newItem) {
+//     return this.items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     const itemIndexToRemove = this.items.findIndex((item) => item === itemToRemove);
+//     return this.items.splice(itemIndexToRemove, 1);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 
-
+// 5.11
 
