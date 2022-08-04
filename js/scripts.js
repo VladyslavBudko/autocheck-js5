89266d48b8b2f4b0e037ad8759eba1a44db69678
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-04 21:16:24 +0300

    5.5

--- js/scripts.js
+++ js/scripts.js
@@ -99,3 +99,26 @@ child.age = 27;
 
 
 // 5.5
+
+const ancestor = {
+  name: "Paul",
+  age: 83,
+  surname: "Dawson",
+  heritage: "Irish",
+};
+// Change code below this line
+
+  const parent = Object.create(ancestor);
+parent.name = "Stacey";
+parent.surname = "Moore";
+parent.age = 54;
+
+  const child = Object.create(parent);
+child.name = "Jason";
+child.age = 27;
+
+// Change code above this line
+
+
+// 5.6
+
