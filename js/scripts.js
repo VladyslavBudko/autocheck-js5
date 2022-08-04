5346952c1ebbd508b17b9c9152be238f6ed02c93
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-04 21:26:37 +0300

    5.9

--- js/scripts.js
+++ js/scripts.js
@@ -151,6 +151,26 @@ class Car {
 
 
 // 5.9
+class Car {
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  // Change code below this line
+getPrice () {
+return this.price;
+}
+
+  changePrice (newPrice) {
+    this.price = newPrice;
+  }
+
+  // Change code above this line
+}
+
+
+// 5.10
 
 
 
