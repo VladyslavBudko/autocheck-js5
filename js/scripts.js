b58df700a47339037fd656710b202bd29809cdc2
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-04 21:14:01 +0300

    5.4

--- js/scripts.js
+++ js/scripts.js
@@ -80,3 +80,22 @@ const historyService = {
   };
 
  
+//   5.4
+
+const parent = {
+  name: "Stacey",
+  surname: "Moore",
+  age: 54,
+  heritage: "Irish",
+};
+// Change code below this line
+
+  const child = Object.create(parent);
+
+// Change code above this line
+child.name = "Jason";
+child.age = 27;
+
+
+
+// 5.5
