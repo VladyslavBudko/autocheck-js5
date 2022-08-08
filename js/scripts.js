c1957372437423f05f9af79e734feacffc6f7f23
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-08 13:56:32 +0300

    5.18

--- js/scripts.js
+++ js/scripts.js
@@ -414,5 +414,28 @@
 // console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
 // console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
 
-
 // 5.18
+
+class User {
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+// Change code below this line
+class Admin extends User {
+  static AccessLevel  = {
+    BASIC: "basic";
+    SUPERUSER: "superuser";
+  };
+}
+
+
+// 5.19
