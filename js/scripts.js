b129acc6ebbe45b73d56e2390ab91954225d5978
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-10 23:11:47 +0300

    Рекурсивний обхід

--- js/scripts.js
+++ js/scripts.js
@@ -564,3 +564,30 @@ function pow(x, n) {
 
   return result;
 }
+
+
+
+
+
+// Рекурсивний обхід
+
+let company = { // той же об’єкт, стиснутий для компактності
+  sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 1600 }],
+  development: {
+    sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+    internals: [{name: 'Jack', salary: 1300}]
+  }
+};
+
+// Функція для підрахунку суми зарплат
+function sumSalaries(department) {
+  if (Array.isArray(department)) { // випадок (1)
+    return department.reduce((prev, current) => prev + current.salary, 0); // сума масиву
+  } else { // випадок (2)
+    let sum = 0;
+    for (let subdep of Object.values(department)) {
+      sum += sumSalaries(subdep); // рекурсивно викликається для підвідділів, суммуючи результат
+    }
+    return sum;
+  }
+}
